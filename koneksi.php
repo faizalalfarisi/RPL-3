973ff3d637aa8ee00840e175caaf016c2816a000
Faizal Alfarisi <faizal.alfarisi@gmail.com>
2016-01-31 22:41:07 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php 
-					$host = "localhost"; 
-					$user = "root"; 
-					$pass = ""; 
-					$db = "restoran"; 
+					$host = "ap-cdbr-azure-southeast-b.cloudapp.net"; 
+					$user = "b50418be293645"; 
+					$pass = "e840ede4"; 
+					$db = "db_broto_azhari"; 
 					$konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! '); 
 					mysql_select_db($db); 
 ?>
